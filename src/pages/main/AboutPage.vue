acffe5fce45144363644fea7b6bdb127c0b99bf6
Nuri Akman <nuriakman@gmail.com>
2024-02-12 16:48:32 +0300

    Hakkımızda sayfası eklendi

--- src/pages/main/AboutPage.vue
+++ src/pages/main/AboutPage.vue
@@ -1,6 +1,24 @@
 <template>
-  <q-page padding>
-    <h1>main HakkımızdaPage</h1>
+  <q-page class="row justify-evenly" padding>
+    <q-card class="q-pa-xl q-ma-md" bordered>
+      <p class="text-h5">Hakkımızda</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, animi
+        minima ad voluptas, illum iusto tempore nisi sunt explicabo omnis modi
+        qui? Culpa voluptas, itaque ipsa quis et nisi facere?
+      </p>
+      <ul>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+        <li>
+          Labore, temporibus fugit illum a, impedit dolore architecto pariatur
+          tenetur,
+        </li>
+        <li>
+          consequuntur aperiam quis voluptate. Recusandae, magni? Perspiciatis
+          asperiores omnis ipsam iste dolorem.
+        </li>
+      </ul>
+    </q-card>
   </q-page>
 </template>
 

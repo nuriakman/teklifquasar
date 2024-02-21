54cc7c9b34884c036a2394fd9fb17b7485a49c4f
Nuri Akman <nuriakman@gmail.com>
2024-02-21 14:38:21 +0300

    Dash bilgileri kaldırıldı

--- src/pages/main/IndexPage.vue
+++ src/pages/main/IndexPage.vue
@@ -1,22 +1,10 @@
 <template>
   <q-page padding>
-    <div class="row items-start q-gutter-md no-wrap">
-      <dashHeader text="Hedef" number="140" icon="person" />
-      <dashHeader text="Üye" number="750" icon="lock" />
-      <dashHeader text="Satış" number="400" icon="money" />
-      <dashHeader
-        text="Gizlenecek"
-        number="400"
-        icon="money"
-        :class="{ hidden: $q.screen.lt.lg ? 'none' : '' }"
-      />
-    </div>
-    <h1>Main IndexPage11 {{ $q.screen.width }}</h1>
+    <dashHeaderList />
+    <h1>Main IndexPage</h1>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import dashHeader from 'src/components/dashHeader.vue';
-import { useQuasar } from 'quasar';
-const $q = useQuasar();
+import dashHeaderList from 'src/components/dashHeaderList.vue';
 </script>
